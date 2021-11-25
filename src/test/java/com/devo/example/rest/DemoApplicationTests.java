7426f8adba6635c3166de21c29b539736bab0506
devo0408 <vovk.dev@gmail.com>
2021-11-25 16:15:42 +0200

    Initial commit

---
+++ src/test/java/com/devo/example/rest/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devo.example.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
